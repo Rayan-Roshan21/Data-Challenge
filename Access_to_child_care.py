20f04d93cfd75bf926b68f61b5d7f8d4d9254ae0
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-04 12:14:45 -0400

    Changed the filter

--- Access_to_child_care.py
+++ Access_to_child_care.py
@@ -17,14 +17,18 @@ df['low_access'] = (
 )
 
 # Filter the rows where CMAUID is 535. This is representing the GTA.
-df_gta = df[df['CMAUID'] == 535]
+df_gta_low_access = df[
+    (df['CMAUID'] == 535) & 
+    (df['low_access'] == True)
+]
+
 
 # Here we're visualizing the data based on a true and false system. 
 # Specifically, if true then it means that a row's values are below or equal to both thresholds and thus, low access to child care services.
 # If false, meaning better access to at least one of the services.
 plt.figure(figsize=(10, 6))
 sns.scatterplot(
-    data=df_gta,
+    data=df_gta_low_access,
     x='public_ef',
     y='walk_ef',
     hue='low_access',
@@ -33,7 +37,7 @@ sns.scatterplot(
 )
 
 # This is used to count categories
-low_access_counts_535 = df_gta['low_access'].value_counts()
+low_access_counts_535 = df_gta_low_access['low_access'].value_counts()
 
 # This is used to present the amount of data points have/have not any access.
 for i, (label, count) in enumerate(low_access_counts_535.items()):
@@ -56,6 +60,6 @@ plt.grid(True)
 plt.show()
 
 # Count number of low and non-low access neighborhoods for CMAUID 535
-low_access_counts_535 = df_gta['low_access'].value_counts()
+low_access_counts_535 = df_gta_low_access,['low_access'].value_counts()
 print("\nLow Access Counts for CMAUID = 535:")
 print(low_access_counts_535)
